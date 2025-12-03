74fa66d84a248d4f706b9240c175b641b7f4819a
JinMyeong Choi <ymo6538@gmail.com>
2025-12-03 16:43:12 +0900

    fix: className 지우기

--- src/compenents/ContentCard.jsx
+++ src/compenents/ContentCard.jsx
@@ -1,13 +1,13 @@
 const imgBaseUrl = "https://image.tmdb.org/t/p/w500";
 
-export default function MovieCard({ title, rating, posterPath }) {
+export default function ContentCard({ title, rating, posterPath }) {
   const imageUrl = imgBaseUrl + posterPath;
 
   return (
-    <div className="movie-card">
-      <img className="movie-card__poster" src={imageUrl} alt={title} />
-      <h3 className="movie-card__title">{title}</h3>
-      <p className="movie-card__rating">평점: {rating}</p>
+    <div>
+      <img src={imageUrl} alt={title} />
+      <h3>{title}</h3>
+      <p>평점: {rating}</p>
     </div>
   );
 }
