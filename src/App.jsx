9d3f3d7bcbfdaaff1e84c55f41665f5ad75b2ba3
JinMyeong Choi <ymo6538@gmail.com>
2025-12-03 16:43:58 +0900

    feat: 무비 목록, 정보 가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import movieListData from "./data/movieListData.json";
 import "./App.css";
+import ContentCard from "./compenents/ContentCard";
 
 function App() {
+  const [movies] = useState(movieListData.results);
   return (
     <div>
-      <div></div>
+      <h1>영화 목록</h1>
+      {movies.map((movie) => (
+        <ContentCard
+          key={movie.id}
+          title={movie.title}
+          rating={movie.vote_average}
+          posterPath={movie.poster_path}
+        />
+      ))}
     </div>
   );
 }
